19a3438bddfee3538b1df6e9f1894fd6b79d9208
James Easter <jameseaster@Jamess-MacBook-Pro.local>
2021-03-26 15:54:19 -0600

    update image file paths

--- src/Components/ImageCarousel.tsx
+++ src/Components/ImageCarousel.tsx
@@ -11,8 +11,8 @@ import { Carousel, Image } from "antd";
  */
 export const ImageCarousel: React.FC<{}> = () => (
   <Carousel autoplay dotPosition={"bottom"}>
-    <Image width={"850px"} src="assets/couch-sessions-1.jpg" />
-    <Image width={"400px"} src="assets/ochancey1.jpg" />
-    <Image width={"900px"} src="assets/ochancey2.jpg" />
+    <Image width={"230px"} src="assets/images/3x4-gsg04.jpg" />
+    <Image width={"230px"} src="assets/images/3x4-ochancey01.png" />
+    <Image width={"230px"} src="assets/images/3x4-ochancey06.jpeg" />
   </Carousel>
 );
--- src/Components/ImageGroup.tsx
+++ src/Components/ImageGroup.tsx
@@ -4,6 +4,15 @@ import React from "react";
 // Ant Design Imports
 import { Image, Row, Col } from "antd";
 
+// Images
+import gsg01 from "../assets/images/3x2-gsg01.jpg";
+import gsg04 from "../assets/images/3x4-gsg04.jpg";
+import gsg06 from "../assets/images/3x2-gsg06.jpg";
+import ochancey01 from "../assets/images/3x4-ochancey01.png";
+import ochancey04 from "../assets/images/3x4-ochancey04.jpg";
+import ochancey05 from "../assets/images/3x4-ochancey05.jpg";
+import ochancey06 from "../assets/images/3x4-ochancey06.jpeg";
+
 /**
  * ImageGroup
  *
@@ -13,31 +22,27 @@ export const ImageGroup: React.FC<{}> = () => (
   <Image.PreviewGroup>
     <Row justify="center" style={{ marginBottom: "-6px", width: "100%" }}>
       <Col xs={24} lg={6}>
-        <Image width={"230px"} src="assets/3x4-gsg04.jpg" />
+        <Image width={"230px"} src={gsg04} />
       </Col>
 
       <Col xs={24} lg={6}>
-        <Image width={"230px"} src="assets/3x4-ochancey01.png" />
+        <Image width={"230px"} src={ochancey01} />
       </Col>
 
       <Col xs={24} lg={6}>
-        <Image width={"230px"} src="assets/3x4-ochancey06.jpeg" />
+        <Image width={"230px"} src={ochancey06} />
       </Col>
     </Row>
     <Row justify="center">
       <Col xs={24} lg={6}>
-        <Image width={"230px"} src="assets/3x4-ochancey04.jpg" />
+        <Image width={"230px"} src={ochancey04} />
       </Col>
       <Col xs={24} sm={6}>
-        <Image width={"230px"} src="assets/3x2-gsg01.jpg" />
-        <Image
-          style={{ marginTop: "-6px" }}
-          width={"230px"}
-          src="assets/3x2-gsg06.jpg"
-        />
+        <Image width={"230px"} src={gsg01} />
+        <Image style={{ marginTop: "-6px" }} width={"230px"} src={gsg06} />
       </Col>
       <Col xs={24} lg={6}>
-        <Image width={"230px"} src="assets/3x4-ochancey05.jpg" />
+        <Image width={"230px"} src={ochancey05} />
       </Col>
     </Row>
   </Image.PreviewGroup>

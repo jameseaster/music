df596e308037955ef1539872935743dc49ff2cd1
James Easter <jameseaster@Jamess-MacBook-Pro.local>
2021-03-26 18:17:38 -0600

    update file structure and paths

---
+++ src/assets/tracks.ts
@@ -0,0 +1,13 @@
+// Audio track images
+import photo from "../assets/1x1_plant.jpg";
+
+// List of tracks that are passed to audio player
+export const tracks = [
+  {
+    title: "Day Dreamer",
+    artist: "James Easter",
+    audioSrc: "/tracks/DayDreamer.mp3",
+    image: photo,
+    color: "dodgerblue",
+  },
+];

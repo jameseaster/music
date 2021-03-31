1024d36d8cbbce7537d2f9b49e88e440687751c0
jameseaster <james.m.easter@gmail.com>
2021-03-30 23:38:47 -0600

    add loading, confirmation, & clear contact form

--- src/Components/EmailForm.tsx
+++ src/Components/EmailForm.tsx
@@ -1,10 +1,11 @@
 // React Imports
-import React from "react";
+import React, { useState } from "react";
 import emailjs from "emailjs-com";
 import axios from "axios";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, Modal } from "antd";
 
 /* eslint-disable no-template-curly-in-string */
+// TODO:
 const validateMessages = {
   required: "${label} is required!",
   types: {
@@ -20,36 +21,61 @@ const validateMessages = {
  * once the users submits the message
  */
 export const EmailForm: React.FC<{}> = () => {
-  // TODO: loading variable
   // TODO: add CAPTCHA support
   // TODO: add auto reply
-  // TODO: confirmation of email sent
-  // TODO: clear form
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
 
+  // Handle form submition
   const onFinish = (values: any) => {
+    // Prevent submitting another message
+    setLoading(true);
+
+    // Values from form
     const templateParams = {
       from_name: values.name,
       reply_to: values.email,
       message: values.message,
     };
 
+    // Get credentials
     axios("/.netlify/functions/sendEmail").then((res: any) => {
       const { service_id, template_id, user_id } = res.data;
 
+      // Send email
       emailjs
         .send(service_id, template_id, templateParams, user_id)
-        .then((result) => console.log(result.status))
-        .catch((error) => console.log(error));
+        .then((result) => {
+          successModal();
+          setLoading(false);
+          form.resetFields();
+          console.log(result.status);
+        })
+        .catch((error) => {
+          errorModal();
+          setLoading(false);
+          console.log(error.status);
+        });
     });
   };
 
+  // Success confirmation
+  function successModal() {
+    Modal.success({ content: "Your messagae was sent!" });
+  }
+  // Error confirmation
+  function errorModal() {
+    Modal.error({ content: "Message failed to send." });
+  }
+
   return (
     <Form
+      form={form}
       layout="vertical"
       name="nest-messages"
       onFinish={onFinish}
-      validateMessages={validateMessages}
       style={{ width: "100%" }}
+      validateMessages={validateMessages}
     >
       <Form.Item name={["name"]} label="Name" rules={[{ required: true }]}>
         <Input />
@@ -67,7 +93,7 @@ export const EmailForm: React.FC<{}> = () => {
         />
       </Form.Item>
       <Form.Item>
-        <Button type="primary" htmlType="submit">
+        <Button loading={loading} type="primary" htmlType="submit">
           Submit
         </Button>
       </Form.Item>

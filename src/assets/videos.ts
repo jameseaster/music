9b501cca4a29a58e81bd586614d9207535b5fc9b
jameseaster <james.m.easter@gmail.com>
2021-04-02 14:17:45 -0600

    add videos.ts and pdf files

---
+++ src/assets/videos.ts
@@ -0,0 +1,22 @@
+import Peter_Bernstein_Lady_Bird from "./pdf/Peter_Bernstein_Lady_Bird.pdf";
+import Wes_Montgomery_Satin_Doll from "./pdf/Wes_Montgomery_Satin_Doll.pdf";
+import Miles_Davis_Milestones_old from "./pdf/Miles_Davis_Milestones_old.pdf";
+
+// List of vidos that are passed to video player
+export const videos = [
+  {
+    title: "Peter Berstien - Lady Bird",
+    url: "https://www.youtube.com/watch?v=7Dy6AteuOHE",
+    pdf: Peter_Bernstein_Lady_Bird,
+  },
+  {
+    title: "Wes Montgomery - Satin Doll",
+    url: "https://www.youtube.com/watch?v=lGysbDsvNOU",
+    pdf: Wes_Montgomery_Satin_Doll,
+  },
+  {
+    title: "Miles Davis - Milestones (old)",
+    url: "https://www.youtube.com/watch?v=z3FH5_C_pzo",
+    pdf: Miles_Davis_Milestones_old,
+  },
+];
